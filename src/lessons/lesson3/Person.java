fac4ef7a8d266796bb8476885f1dc2fe1af3fb90
yurakrushh <yurakrushrob@gmail.com>
2022-04-10 15:55:50 +0300

    Class Work

---
+++ src/lessons/lesson3/Person.java
@@ -0,0 +1,20 @@
+package lessons.lesson3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Person {
+
+    public String name;
+
+
+};
+
---
+++ src/lessons/lesson3/Pet.java
@@ -0,0 +1,16 @@
+package lessons.lesson3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Pet {
+    public String name;
+}
